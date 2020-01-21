df0b74ba8fb4cbcacb3ce543625b834b539d872f
Cat Chen <catchen@catchen.me>
2020-01-20 19:41:06 -0800

    Top bar in App

--- src/App.js
+++ src/App.js
@@ -7,17 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className="App-title">Around</h1>
       </header>
     </div>
   );
