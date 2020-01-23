b801c59794aee3cee7f6fb85595bf0a64609316b
Cat Chen <catchen@catchen.me>
2020-01-22 21:32:51 -0800

    Set up logout link

--- src/components/Topbar.js
+++ src/components/Topbar.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import { Icon } from 'antd';
 import logo from '../assets/logo.svg';
 import '../styles/Topbar.css';
 
-export function Topbar() {
+export function Topbar(props) {
   return (
     <header className="App-header">
       <img src={logo} className="App-logo" alt="logo" />
       <h1 className="App-title">Around</h1>
+      {
+        props.isLoggedIn ?
+          <a onClick={props.handleLogout} className="logout">
+            <Icon type="logout" />
+            {' '}Logout
+          </a> :
+          null
+      }
     </header>
   );
 }
