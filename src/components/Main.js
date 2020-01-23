52d780b1fcf1b8c4c837025ad494e06e0c6f9139
Cat Chen <catchen@catchen.me>
2020-01-22 19:34:27 -0800

    Introduced Login

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Register } from './Register';
+import { Login } from './Login';
 import '../styles/Main.css';
 
 export function Main() {
   return (
     <div className="main">
-      <Register />
+      <Login />
     </div>
   );
 }
