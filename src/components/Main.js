0f50ea221f5cdfbdc353818e3a228484c68004e5
Cat Chen <catchen@catchen.me>
2020-01-22 19:52:53 -0800

    Set up Switch and Route

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import { Register } from './Register';
 import { Login } from './Login';
+import { Switch, Route } from 'react-router-dom'
 import '../styles/Main.css';
 
 export function Main() {
   return (
     <div className="main">
-      <Login />
+      <Switch>
+        <Route path="/login" component={Login} />
+        <Route path="/register" component={Register} />
+        <Route component={Login} />
+      </Switch>
     </div>
   );
 }
