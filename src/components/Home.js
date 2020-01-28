92adc6e4cc28e63962866ac4b353e6cd66a47f06
Cat Chen <catchen@catchen.me>
2020-01-27 16:45:14 -0800

    [4.0] Added tabs in Home
    Added Home CSS

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,24 @@
 import React from 'react';
+import { Tabs, Button } from 'antd';
 import '../styles/Home.css';
 
-export function Home() {
-  return (<div>Home</div>);
+const { TabPane } = Tabs;
+
+export class Home extends React.Component {
+  render() {
+    const operations = <Button>Create New Post</Button>;
+    return (
+      <Tabs tabBarExtraContent={operations} className="main-tabs">
+        <TabPane tab="Tab 1" key="1">
+          Content of tab 1
+        </TabPane>
+        <TabPane tab="Tab 2" key="2">
+          Content of tab 2
+        </TabPane>
+        <TabPane tab="Tab 3" key="3">
+          Content of tab 3
+        </TabPane>
+      </Tabs>
+    );
+  }
 };
