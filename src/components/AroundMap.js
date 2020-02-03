62d0880ccfc64dc9e907a3eaabfd1e33611c33df
Cat Chen <catchen@catchen.me>
2020-02-03 14:58:16 -0800

    [6.6] Responding to map change

--- src/components/AroundMap.js
+++ src/components/AroundMap.js
@@ -20,13 +20,35 @@ class NormalAroundMap extends React.Component {
     }))
   }
 
+  reloadMarkers = () => {
+    const center = this.map.getCenter();
+    const position = { latitude: center.lat(), longitude: center.lng() };
+
+    const bounds = this.map.getBounds();
+    const northEast = bounds.getNorthEast();
+    const east = new window.google.maps.LatLng(center.lat(), northEast.lng());
+    const range =
+      window.google.maps.geometry.spherical.computeDistanceBetween(center, east)
+      / 1000;
+
+    this.props.onChange(position, range);
+  }
+
+  saveMapRef = (mapInstance) => {
+    this.map = mapInstance;
+    window.map = mapInstance;
+  }
+
   render() {
     const position = JSON.parse(localStorage.getItem(POSITION_KEY));
 
     return (
       <GoogleMap
+        ref={this.saveMapRef}
         defaultZoom={11}
         defaultCenter={{ lat: position.latitude, lng: position.longitude }}
+        onDragEnd={this.reloadMarkers}
+        onZoomChanged={this.reloadMarkers}
       >
         {this.props.posts.map((post) => (
           <AroundMarker
--- src/components/Home.js
+++ src/components/Home.js
@@ -167,6 +167,7 @@ export class Home extends React.Component {
             containerElement={<div style={{ height: `600px` }} />}
             mapElement={<div style={{ height: `100%` }} />}
             posts={this.state.posts}
+            onChange={this.loadNearbyPost}
           />
         </TabPane>
       </Tabs>
